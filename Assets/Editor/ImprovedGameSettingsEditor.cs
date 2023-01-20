239da177eebdc9fe75c80a28475c2d0a2bd99bc5
TriKnot <johan.brandt91@gmail.com>
2023-01-20 13:32:30 +0100

    Added support for MinMaxSlider in createfield method

--- Assets/Editor/ImprovedGameSettingsEditor.cs
+++ Assets/Editor/ImprovedGameSettingsEditor.cs
@@ -36,6 +36,14 @@ namespace Editor
                 var shipFoldout = GetShipFoldout(ship);
                 _root.Add(shipFoldout);
             }
+            
+            AsteroidSettings[] asteroidSettingsArray = GetAssetsOfType<AsteroidSettings>();
+
+            foreach (var asteroid in asteroidSettingsArray)
+            {
+                var asteroidFoldout = GetAsteroidFoldout(asteroid);
+                _root.Add(asteroidFoldout);
+            }
         }
         
         private Foldout GetShipFoldout(ShipSettings shipSettings)
@@ -49,30 +57,19 @@ namespace Editor
 
             SerializedObject shipSO = new SerializedObject(shipSettings);
 
-            PropertyField pfThrottle = CreatePropertyField(shipSO, "Throttle");
+            var pfThrottle = CreateField(shipSO, "Throttle");
             shipFoldout.Add(pfThrottle);
             
-            PropertyField pfRotation = CreatePropertyField(shipSO, "Rotation");
+            var pfRotation = CreateField(shipSO, "Rotation");
             shipFoldout.Add(pfRotation);
            
             SerializedObject so = new SerializedObject(shipSettings.Health);
             
-            PropertyField pfHealth = CreatePropertyField(so, "IntValue", "Starting Health");
+            var pfHealth = CreateField(so, "IntValue", "Starting Health");
             shipFoldout.Add(pfHealth);
             
             return shipFoldout;
             
-        }
-
-        private PropertyField CreatePropertyField(SerializedObject so, string propertyName, string label = "")
-        {
-            if(string.IsNullOrEmpty(label))
-                label = propertyName;
-            
-            SerializedProperty sp = so.FindProperty(propertyName);
-            PropertyField pf = new PropertyField(sp, label);
-            pf.Bind(so);
-            return pf;
         }
 
         private Foldout GetAsteroidFoldout(AsteroidSettings asteroidSettings)
@@ -83,18 +80,60 @@ namespace Editor
             Label shipNameLabel = new Label("Asteroids");
             shipNameLabel.AddToClassList("header");
             asteroidFoldout.Add(shipNameLabel);
+            
+            SerializedObject so = new SerializedObject(asteroidSettings);
 
             //MinMax Force
+            var pfForce = CreateField(so, "Force");
+            asteroidFoldout.Add(pfForce);
             //MinMax Size
+            var pfSize = CreateField(so, "Size");
+            asteroidFoldout.Add(pfSize);
             //MinMax Torque
-            //MinMax SpawnRate
-            
-            
-            
+            var pfTorque = CreateField(so, "Torque");
+            asteroidFoldout.Add(pfTorque);
 
             return asteroidFoldout;
         }
-
+        
+        private VisualElement CreateField(SerializedObject so, string propertyName, string label = "")
+        {
+            if(string.IsNullOrEmpty(label))
+                label = propertyName;
+            
+            SerializedProperty sp = so.FindProperty(propertyName);
+            VisualElement ve = new VisualElement();
+            switch (sp.propertyType)
+            {
+                case SerializedPropertyType.Vector2:
+                    ve = new VisualElement();
+                    ve.Add(new Label(label));
+                    
+                    
+                    FloatField minField = new FloatField();
+                    minField.BindProperty(sp.FindPropertyRelative("x"));
+                    ve.Add(minField);
+                    
+                    var slider = new MinMaxSlider();
+                    slider.lowLimit = 0;
+                    slider.highLimit = 100;
+                    slider.BindProperty(sp);
+                    ve.Add(slider);
+                    
+                    FloatField maxField = new FloatField();
+                    maxField.BindProperty(sp.FindPropertyRelative("y"));
+                    ve.Add(maxField);
+                    
+                    break;
+                
+                default:
+                    ve = new PropertyField(sp, label);
+                    break;
+            }
+            ve.Bind(so);
+            return ve;
+        }
+        
         public static T[] GetAssetsOfType<T>() where T : ScriptableObject
         {
             string[] guids = AssetDatabase.FindAssets($"t: {typeof(T).Name}");
